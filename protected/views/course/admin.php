af44144d31c49ed813bbd292dac9d9630503fb04
Ice-Heart <lehoang0211@gmail.com>
2015-12-11 15:42:03 +0700

    hoanglv

--- protected/views/course/admin.php
+++ protected/views/course/admin.php
@@ -26,37 +26,54 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Courses</h1>
+<div id="sidebar">
+<?php 
+	$this->widget('CTreeView',array(
+	        'data'=>$dataTree,
+	        'animated'=>'fast', //quick animation
+	        'collapsed'=>'false',//remember must giving quote for boolean value in here
+	        'htmlOptions'=>array(
+	        	'id'=>'treeview-catg',
+	            'class'=>'treeview-red',//there are some classes that ready to use
+	        ),
+	));
+?>
+</div>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
+<div id="main-content">
+	<h1>Manage Courses</h1>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+	<p>
+	You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+	or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+	</p>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'course-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'ID',
-		'Name',
-		'Description',
-		'Parent_id',
-		'Type',
-		'Major_id',
-		/*
-		'Version',
-		'Status',
-		*/
-		array(
-			'class'=>'CButtonColumn',
+	<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+	<div class="search-form" style="display:none">
+	<?php $this->renderPartial('_search',array(
+		'model'=>$model,
+	)); ?>
+	</div><!-- search-form -->
+
+	<?php $this->widget('zii.widgets.grid.CGridView', array(
+		'id'=>'course-grid',
+		'dataProvider'=>$model->search(),
+		'filter'=>$model,
+		'columns'=>array(
+			'ID',
+			'Name',
+			'Description',
+			'Parent_id',
+			'Type',
+			'Major_id',
+			/*
+			'Version',
+			'Status',
+			*/
+			array(
+				'class'=>'CButtonColumn',
+			),
 		),
-	),
-)); ?>
+	)); ?>
+
+</div>
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="en">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Admin</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/css/bootstrap.min.css"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/css/bootstrap-theme.min.css"/>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
 	<!--<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/js/bootstrap.min.js"></script>-->
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/dropdown.js"></script>
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/setheight.js"></script>
