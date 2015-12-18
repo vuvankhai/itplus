2d4e70225b5cbb5c336201c105c2aed5d641cfe3
Ice-Heart <lehoang0211@gmail.com>
2015-12-18 13:53:45 +0700

    hoanglv

--- protected/views/course/create.php
+++ protected/views/course/create.php
@@ -14,4 +14,4 @@ $this->menu=array(
 ?>
 
 <h1 class="text-success title">Create Course</h1>
-<?php $this->renderPartial('_form', array('model'=>$model, 'id'=>$id); ?>
+<?php $this->renderPartial('_form', array('model'=>$model, 'id'=>$id)); ?>
