dcd81ae92ae30cef4255d5deb0e867702398132c
LeHoang <lehoang0211@gmail.com>
2016-01-11 20:40:28 -0800

    hoanglv
    sửa lại login

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -5,45 +5,43 @@
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
-class UserIdentity extends CUserIdentity
-{
+class UserIdentity extends CUserIdentity {
 
+    /**
+     * Authenticates a user.
+     * The example implementation makes sure if the username and password
+     * are both 'demo'.
+     * In practical applications, this should be changed to authenticate
+     * against some persistent user identity storage (e.g. database).
+     * @return boolean whether authentication succeeds.
+     */
+    public function authenticate() {
+        
+//        $users=array(
+//                // username => password
+//                'demo'=>'demo',
+//                'admin'=>'admin',
+//        );
+//        if(!isset($users[$this->username]))
+//                $this->errorCode=self::ERROR_USERNAME_INVALID;
+//        elseif($users[$this->username]!==$this->password)
+//                $this->errorCode=self::ERROR_PASSWORD_INVALID;
+//        else
+//                $this->errorCode=self::ERROR_NONE;
+//        return !$this->errorCode;
 
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-            
-//            $users=array(
-//                    // username => password
-//                    'demo'=>'demo',
-//                    'admin'=>'admin',
-//            );
-//            if(!isset($users[$this->username]))
-//                    $this->errorCode=self::ERROR_USERNAME_INVALID;
-//            elseif($users[$this->username]!==$this->password)
-//                    $this->errorCode=self::ERROR_PASSWORD_INVALID;
-//            else
-//                    $this->errorCode=self::ERROR_NONE;
-//            return !$this->errorCode;
-            
-		$user = Account::model()->find('Username=:Username', array('Username'=>$this->username));
-		
-		if(!isset($user->Username))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($user->Password!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
+        $user = Account::model()->find('Username=:Username', array('Username' => $this->username));
+        
+        if ($user === null)
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        else if (isset($user->Password) && $user->Password != $this->password)
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        else{
+            $this->errorCode = self::ERROR_NONE;
+            Yii::app()->user->setState('idAccount', $user->ID);
+        }
 
-		$this->setState('idAccount', $user->ID);
-		
-		return !$this->errorCode;
-	}
+        return !$this->errorCode;
+    }
+
 }
